8c7a4424b9ec6e1db31035444c903c7b900741a2
Drakson-Santos <darksonbezerra123@gmail.com>
2022-01-15 18:50:14 -0300

    install log

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('combined'))
 
 require("./routes/test")(app);
 require("./routes/projectsRoutes")(app);
